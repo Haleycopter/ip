1c7614f8c990b06ec97a48534632875dcd4db65d
Haleycopter <102632714+Haleycopter@users.noreply.github.com>
2026-01-14 15:05:56 +0800

    Add ability to store tasks in list command

--- src/main/java/Howly.java
+++ src/main/java/Howly.java
@@ -10,19 +10,32 @@ public class Howly {
                 + "____________________________________________________________\n";
         System.out.println(welcomeMsg);
 
+        String[] tasks = new String[100]; //fixed-size array to store tasks
+        int taskCount = 0;
+
         while (true) {
             String userInput = scanner.nextLine();
             if (userInput.equals("bye")) {
                 String exitMsg = "____________________________________________________________\n"
                         + " Bye. Hope to see you again soon!\n"
-                        + "____________________________________________________________";
+                        + "____________________________________________________________\n";
                 System.out.println(exitMsg);
                 break;
             }
-            String echoMsg = "____________________________________________________________\n"
-                    + " " + userInput + "\n"
-                    + "____________________________________________________________";
-            System.out.println(echoMsg);
+
+            if (userInput.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = userInput;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + userInput);
+                System.out.println("____________________________________________________________");
+            }
         }
     }
 }
