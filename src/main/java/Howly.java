264e4274f3d46de61fc820cb397045fc3c152376
Haleycopter <102632714+Haleycopter@users.noreply.github.com>
2026-01-14 15:24:52 +0800

    Added class Task and mark/unmark functionality

--- src/main/java/Howly.java
+++ src/main/java/Howly.java
@@ -10,11 +10,11 @@ public class Howly {
                 + "____________________________________________________________\n";
         System.out.println(welcomeMsg);
 
-        String[] tasks = new String[100]; //fixed-size array to store tasks
+        Task[] tasks = new Task[100]; //fixed-size array to store tasks
         int taskCount = 0;
 
         while (true) {
-            String userInput = scanner.nextLine();
+            String userInput = scanner.nextLine().trim();
             if (userInput.equals("bye")) {
                 String exitMsg = "____________________________________________________________\n"
                         + " Bye. Hope to see you again soon!\n"
@@ -25,12 +25,31 @@ public class Howly {
 
             if (userInput.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println(" " + (i + 1) + ". " + tasks[i]);
                 }
                 System.out.println("____________________________________________________________");
-            } else {
-                tasks[taskCount] = userInput;
+            } else if (userInput.startsWith("mark ")) {
+                int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                if (index >= 0 && index < taskCount) {
+                    tasks[index].markAsDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println("   " + tasks[index]);
+                    System.out.println("____________________________________________________________");
+                }
+            } else if (userInput.startsWith("unmark ")) {
+                int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                if (index >= 0 && index < taskCount) {
+                    tasks[index].markAsNotDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                    System.out.println("   " + tasks[index]);
+                    System.out.println("____________________________________________________________");
+                }
+            } else { //add a new task
+                tasks[taskCount] = new Task(userInput);
                 taskCount++;
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + userInput);
