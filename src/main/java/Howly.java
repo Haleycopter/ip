30bc6e74eae5c49a2ad1d2c1d2b03f05a4b8f38f
Haleycopter <102632714+Haleycopter@users.noreply.github.com>
2026-01-14 14:52:24 +0800

    Add support for echo

--- src/main/java/Howly.java
+++ src/main/java/Howly.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class Howly {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
+        Scanner scanner = new Scanner(System.in);
+
+        String welcomeMsg = "____________________________________________________________\n"
                 + " Hello! I'm Howly\n"
                 + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
-        System.out.println(logo);
+                + "____________________________________________________________\n";
+        System.out.println(welcomeMsg);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                String exitMsg = "____________________________________________________________\n"
+                        + " Bye. Hope to see you again soon!\n"
+                        + "____________________________________________________________";
+                System.out.println(exitMsg);
+                break;
+            }
+            String echoMsg = "____________________________________________________________\n"
+                    + " " + userInput + "\n"
+                    + "____________________________________________________________";
+            System.out.println(echoMsg);
+        }
     }
 }
