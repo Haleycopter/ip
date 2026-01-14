73d96f58da5a42c7a79e3fd3a275576c8f03d20b
Haleycopter <102632714+Haleycopter@users.noreply.github.com>
2026-01-14 14:20:27 +0800

    Add greeting

--- src/main/java/Howly.java
+++ src/main/java/Howly.java
@@ -1,7 +1,7 @@
 public class Howly {
     public static void main(String[] args) {
         String logo = "____________________________________________________________\n"
-                + " Hello! I'm [YOUR CHATBOT NAME]\n"
+                + " Hello! I'm Howly\n"
                 + " What can I do for you?\n"
                 + "____________________________________________________________\n"
                 + " Bye. Hope to see you again soon!\n"
