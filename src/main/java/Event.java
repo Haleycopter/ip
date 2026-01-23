63bce395c6326b55c50296ff0b37b8438df08d59
Haleycopter <102632714+Haleycopter@users.noreply.github.com>
2026-01-23 13:17:50 +0800

    Modify Event task to understand and store date & time input

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,14 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 class Event extends Task {
-    private final String from;
-    private final String to;
+    private final LocalDate from;
+    private final LocalDate to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDate.parse(from);
+        this.to = LocalDate.parse(to);
     }
 
     @Override
@@ -15,6 +18,7 @@ class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to +")";
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MMM d yyyy");
+        return "[E]" + super.toString() + " (from: " + from.format(fmt) + " to: " + to.format(fmt) +")";
     }
 }
